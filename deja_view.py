60532d3be10de429ae72447d10999310403f06d0
ramen0x3f <ramen0x3f@users.noreply.github.com>
2022-04-01 13:29:19 -0400

    Update deja_view.py

--- deja_view.py
+++ deja_view.py
@@ -17,7 +17,7 @@ META[
 		  /___/                                  \n"""
 META["AUTHOR"] = "Alyssa Rahman @ramen0x3f"
 META["CREATED"] = "2022-03-29"
-META["LASTUPDATED"] = "2022-03-29"
+META["LASTUPDATED"] = "2022-04-01"
 
 META["DESCRIPTION"] = """Simple wrapper to read in ViewStates from event logs and bulk decrypt them using viewgen. Note: I renamed viewgen to viewgen.py for this to work."""
 META["EXAMPLES"] = """python3 deja_view.py -h"""
@@ -27,6 +27,7 @@ from base64 import b64decode
 from binascii import unhexlify
 from csv import DictWriter
 from hashlib import md5
+from re import search
 from sys import stderr
 from viewgen import read_webconfig, ViewGen
 
@@ -38,9 +39,11 @@ def decrypt_all(args):
 		results.extend(get_events(args.logs))
 		for e in results:
 			e['Decrypted ViewState'] = viewgen.decrypt(e['ViewState'])[0].decode('ascii')
-			e['Payload File Hash'] = get_executable(e['Decrypted ViewState'])
+			e['Payload File Hash'] = get_executable(b64decode(e['Decrypted ViewState']))
+			e['Suspicious Gadgets'] = get_gadgets(b64decode(e['Decrypted ViewState']))
 			
 	if args.payload:
+		decrypted = viewgen.decrypt(args.payload).decode('ascii')
 		results.append({
 			"Server Hostname": "N/A",
 			"Server Username": "N/A",
@@ -48,8 +51,9 @@ def decrypt_all(args):
 			"Source IP": "N/A",
 			"User-Agent": "N/A",
 			"ViewState": args.payload,
-			"Decrypted ViewState": viewgen.decrypt(args.payload).decode('ascii'),
-			"Payload File Hash": get_executable(viewgen.decrypt(args.payload).decode('ascii'))
+			"Decrypted ViewState": decrypted,
+			"Payload File Hash": get_executable(b64decode(decrypted)),
+			"Suspicious Gadgets": get_gadgets(b64decode(decrypted))
 		})
 
 	return results
@@ -73,11 +77,10 @@ def get_events(log_path):
 	return events
 
 def get_executable(viewstate):
-	decoded = b64decode(viewstate)
-	offset = decoded.find(b'\x4d\x5a\x90')
+	offset = viewstate.find(b'\x4d\x5a\x90')
 
 	if offset != -1:
-		exe = decoded[offset:]
+		exe = viewstate[offset:]
 		exe_hash = md5(exe).hexdigest()
 		with open(exe_hash, "wb") as exe_out:
 			exe_out.write(exe)
@@ -85,6 +88,31 @@ def get_executable(viewstate):
 		return exe_hash
 	else:
 		return None
+
+def get_gadgets(viewstate):
+	patterns = { 	'ActivitySurrogateDisableTypeCheck': search(b'GetField.*disableActivitySurrogateSelectorTypeCheck.*SetValue.*setMethod', viewstate),
+					'ActivitySurrogateSelector': search(b'ActivitySurrogateSelector.*ObjectSurrogate.*ObjectSerializedRef', viewstate),
+					'AxHostState': search(b'AxHost.*State.*Process.*Start', viewstate),
+					'ClaimsIdentity': search(b'System.Security.Claims.ClaimsIdentity.*m_serializedClaims', viewstate),
+					'DataSet': search(b'System.Data.DataSet.*DataSet.Namespace.*System.Data.SerializationFormat', viewstate),
+					'ObjectDataProvider2': search(b'ObjectInstance.*ObjectDataProvider', viewstate),
+					'ObjectDataProvider': search(b'ObjectDataProvider.*MethodName', viewstate),
+					'PSObject': search(b'System.Management.Automation.*CimInstance.*RunspaceInvoke', viewstate),
+					'RolePrincipal': search(b'RolePrincipal.*System.Security.ClaimsPrincipal.Identities', viewstate),
+					'SessionSecurityToken': search(b'SecurityContextToken.*ClaimsPrincipal.*BootStrapToken', viewstate),
+					'SessionViewStateHistoryItem': search(b'System.Web.UI.MobileControls.SessionViewState.*SessionViewStateHistoryItem', viewstate),
+					'TextFormattingRunProperties': search(b'TextFormattingRunProperties.*ForegroundBrush', viewstate),
+					'TypeConfuseDelegate': search(b'System.DelegateSerializationHolder.*targetTypeAssembly', viewstate),
+					'TypeConfuseDelegateMono': search(b'System.DelegateSerializationHolder.*System.Reflection.MemberInfoSerializationHolder.*targetTypeAssembly', viewstate),
+					'WindowsIdentity': search(b'WindowsIdentity.*System.Security.ClaimsIdentity.actor', viewstate),
+					'WindowsPrincipal': search(b'WindowsPrincipal.*System.Security.ClaimsIdentity.actor', viewstate) 
+				}
+
+	# Combine results into a string for TSV
+	gadgets = ", ".join(dict(filter(lambda elem: elem[1] is not None, patterns.items())).keys())
+
+	# Return None if none found
+	return gadgets if len(gadgets) > 0 else None
 
 def parse_arguments() -> ArgumentParser:
 	# Setting up argparser
@@ -131,7 +159,8 @@ if __name__ == "__main__":
 	# Stats 
 	success = len([x for x in decrypted if x['Decrypted ViewState'] is not None])
 	exes = len([x for x in decrypted if x['Payload File Hash'] is not None])
-	print(f"[+] Stats\n\tTotal ViewStates: {len(decrypted)}\n\tDecrypted ViewStates: {success}\n\tDecryption Errors: {len(decrypted)-success}\n\tExecutables: {exes}")
+	gadgets = len([x for x in decrypted if x['Suspicious Gadgets'] is not None])
+	print(f"[+] Stats\n\tTotal ViewStates: {len(decrypted)}\n\tDecrypted ViewStates: {success}\n\tDecryption Errors: {len(decrypted)-success}\n\tExecutables: {exes}\n\tSuspicious Gadgets: {gadgets}")
 
 	# Output
 	with open(args.output, "w") as out:
